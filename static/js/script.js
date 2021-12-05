03494483f9cd3a573f1497fad12083a333439224
gercollector <amanattaalaibekov@gmail.com>
2021-12-06 01:45:56 +0600

    add script.js

---
+++ static/js/script.js
@@ -0,0 +1,8 @@
+$('.trpp').on('click',function () {
+    $('.video-object').toggleClass('show');
+    $('.main-video-block').toggleClass('bg-show');
+  });
+//   $('.menu-list-item').on('click',function () {
+//     $('.mobile-btn').removeClass('show');
+//     $('.menu').removeClass('show-menu');
+//   });

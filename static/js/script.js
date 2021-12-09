409db0035d0ea94cec6611b8daa7beaf4be1be83
gercollector <amanattaalaibekov@gmail.com>
2021-12-09 15:02:00 +0600

    burger-menu commit

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,15 @@
+
+$('.mobile-btn').on('click',function () {
+  $('.mobile-btn').toggleClass('btn-show');
+  $('.menu-block-list').toggleClass('menu-show');
+});
+
+$('.menu-block-item').on('click',function () {
+  $('.mobile-btn').removeClass('btn-show');
+  $('.menu-block-list').removeClass('menu-show');
+});
+
 $('.trpp').on('click',function () {
     $('.video-object').toggleClass('show');
     $('.main-video-block').toggleClass('bg-show');
   });
-//   $('.menu-list-item').on('click',function () {
-//     $('.mobile-btn').removeClass('show');
-//     $('.menu').removeClass('show-menu');
-//   });
